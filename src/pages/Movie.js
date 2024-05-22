688561521a968c88e37e5fd8f0e8d5c914dc5697
g;itchtonny <sirmwangitosh@gmail.com>
2024-05-22 20:38:25 +0300

    done

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,40 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch movie');
+        }
+        return response.json();
+      })
+      .then(data => setMovie(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, [id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        <div>
+          {movie.genres.map((genre, index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
       </main>
     </>
   );
